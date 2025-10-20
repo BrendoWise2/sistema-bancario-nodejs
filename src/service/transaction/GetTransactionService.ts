460cefd5a7cbcef8117f864483eaf324df1fcef9
BrendoWise2 <brendo.oliveira@aluno.unincor.edu.br>
2025-10-20 20:04:26 -0300

    Backend robusto - 20:04 (20/10/25)

--- src/service/transaction/GetTransactionService.ts
+++ src/service/transaction/GetTransactionService.ts
@@ -3,19 +3,36 @@ import prismaClient from "../../prisma"
 interface StatementRequest {
     accountId: string;
     user_id: string;
+    startDate?: string;
+    endDate?: string;
+    type?: string,
+
 }
 
 
 
 class GetTransactionService {
-    async execute({ accountId, user_id }: StatementRequest) {
+    async execute({ accountId, user_id, startDate, endDate, type }: StatementRequest) {
+
+        const dataFilters: any = {}
+        const whereConditions: any = {}
+
+        if (startDate) {
+            dataFilters.gte = new Date(startDate)
+        }
+
+        if (endDate) {
+            dataFilters.lte = new Date(endDate)
+        }
 
         const account = await prismaClient.account.findFirst({
 
             where: {
                 id: accountId,
                 userId: user_id,
+
             },
+
         });
 
         if (!account) {
@@ -23,17 +40,29 @@ class GetTransactionService {
 
         }
 
+        whereConditions.OR = [
+            { fromAccountId: accountId },
+            { toAccountId: accountId }
+        ];
+
+
+        if (startDate || endDate) {
+            // Adiciona a chave 'created_at' ao objeto whereConditions, 
+            // usando o objeto dataFilters que acabamos de criar.
+            whereConditions.created_at = dataFilters;
+        }
+
+        if (type) {
+            whereConditions.type = type;
+        }
+
         const statement = await prismaClient.transaction.findMany({
-            where: {
-                OR: [
-                    { fromAccountId: accountId },
-                    { toAccountId: accountId }
-                ]
-            },
+            where: whereConditions, // Usa o objeto completo
             orderBy: {
                 created_at: "desc"
             }
         });
+
 
         return statement;
 
